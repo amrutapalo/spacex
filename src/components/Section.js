b2db22df29bb5e9a4aab9bf10f33a2d2706bde6f
amrutapalo <amrutapalo1997@gmail.com>
2022-08-17 12:29:16 +0530

    scroll-arrow

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,6 +9,16 @@ const Section = (props) => {
         <h2>{props.title}</h2>
         <div className="button">{props.buttonText}</div>
       </div>
+      <div class="scroll-arrow">
+        <svg width="30px" height="20px">
+          <path
+            stroke="#ffffff"
+            fill="none"
+            stroke-width="2px"
+            d="M2.000,5.000 L15.000,18.000 L28.000,5.000 "
+          ></path>
+        </svg>
+      </div>
     </section>
   );
 };
