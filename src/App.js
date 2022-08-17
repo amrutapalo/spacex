49a4a177fac5d3121d4edb100c633c48f8cec16e
amrutapalo <amrutapalo1997@gmail.com>
2022-08-17 12:04:38 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import './App.css';
-import Section from './components/Section';
-import Header from './UI/Header';
-import sectionOne from '/Users/amrutapalo/Desktop/spacex/src/images/section-a.webp'
-import sectionTwo from '/Users/amrutapalo/Desktop/spacex/src/images/section-b.webp'
-import sectionThree from '/Users/amrutapalo/Desktop/spacex/src/images/section-c.webp'
-import sectionFour from '/Users/amrutapalo/Desktop/spacex/src/images/section-d.webp'
+import "./App.css";
+import Section from "./components/Section";
+import Footer from "./UI/Footer";
+import Header from "./UI/Header";
+import sectionOne from "/Users/amrutapalo/Desktop/spacex/src/images/section-a.webp";
+import sectionTwo from "/Users/amrutapalo/Desktop/spacex/src/images/section-b.webp";
+import sectionThree from "/Users/amrutapalo/Desktop/spacex/src/images/section-c.webp";
+import sectionFour from "/Users/amrutapalo/Desktop/spacex/src/images/section-d.webp";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Section title="STARLINK MISSION" subtitle="RECENT LAUNCH" buttonText="REWATCH" image={sectionOne}></Section>
-      <Section title="KPLO MISSION" subtitle="RECENT LAUNCH" buttonText="REWATCH" image={sectionTwo}></Section>
-      <Section title="STARSHIP UPDATE" subtitle="" buttonText="LEARN MORE" image={sectionThree}></Section>
-      <Section title="STARSHIP TO LAND NASA ASTRONAUTS ON THE MOON" subtitle="" buttonText="LEARN MORE" image={sectionFour}></Section>
+      <Section
+        title="STARLINK MISSION"
+        subtitle="RECENT LAUNCH"
+        buttonText="REWATCH"
+        image={sectionOne}
+      ></Section>
+      <Section
+        title="KPLO MISSION"
+        subtitle="RECENT LAUNCH"
+        buttonText="REWATCH"
+        image={sectionTwo}
+      ></Section>
+      <Section
+        title="STARSHIP UPDATE"
+        subtitle=""
+        buttonText="LEARN MORE"
+        image={sectionThree}
+      ></Section>
+      <Section
+        title="STARSHIP TO LAND NASA ASTRONAUTS ON THE MOON"
+        subtitle=""
+        buttonText="LEARN MORE"
+        image={sectionFour}
+      ></Section>
+      <Footer></Footer>
     </div>
   );
 }
