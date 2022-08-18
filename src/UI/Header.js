4cc488f2311bd2015ee2ed811b451e7ec7c409f7
amrutapalo <amrutapalo1997@gmail.com>
2022-08-19 01:14:17 +0530

    mobile-menu and hamburger-cross icon

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import NavBar from './NavBar';
-import './Header.css'
-import logo from '../images/logo.png'
-import Overlay from './Overlay';
-
+import React, { useState } from "react";
+import NavBar from "./NavBar";
+import "./Header.css";
+import logo from "../images/logo.png";
+import Overlay from "./Overlay";
 
 const Header = () => {
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+  const onClickHamburger = () => {
+    setShowHamburgerMenu(true);
+  };
+
+  const onClickCloseButton = () => {
+    setShowHamburgerMenu(false);
+  }
+
   return (
     <div className="header-container">
-        <div className="logo">
-            <img src={logo} alt="logo" />
-        </div>
-        <NavBar/>
-        <div className="hamburger">
-          <div className="hamburger-top"></div>
-          <div className="hamburger-middle"></div>
-          <div className="hamburger-bottom"></div>
-        </div>
-        <Overlay></Overlay>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <NavBar />
+      {!showHamburgerMenu && <div className="hamburger" onClick={onClickHamburger}>
+        <div className="hamburger-top"></div>
+        <div className="hamburger-middle"></div>
+        <div className="hamburger-bottom"></div>
+      </div>}
+      {showHamburgerMenu && <div className="close-button" onClick={onClickCloseButton}>
+        <div className="hamburger-top"></div>
+        <div className="hamburger-bottom"></div>
+      </div>}
+      {showHamburgerMenu && <Overlay></Overlay>}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
